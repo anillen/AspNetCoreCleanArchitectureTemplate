73bf9b14754aa39e2d75d8ab47953587825cc7be
Anillen <daniil_telenkov@vk.com>
2025-11-01 12:02:47 +0300

    feat(architecture): create clean architecture template for ASP.NET Core

---
+++ AspNetCoreCleanArchitectureTemplate.API/Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArchitectureTemplate.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class HomeController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Index() => NoContent();
+}
